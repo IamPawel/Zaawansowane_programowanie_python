6d5e4d1bf2e339ab9d9999e63e0303c35a719fc5
IamPawel <150140921+IamPawel@users.noreply.github.com>
2024-04-08 21:46:17 +0200

    database

---
+++ zad_3.py
@@ -0,0 +1,10 @@
+"""
+Endpoint, który zwraca listę filmów w postaci obiektów json. Kroki do
+wykonania:
+pobrać dane z pliku
+
+- stworzyć klasę Movie (posłuży jako model danych)
+- przeiterować pętlą po wierszach z pliku i stworzyć tyle obiektów klasy Movie ile wierszy
+- wykorzystać metodę magiczną __dict__ do serializacji obiektu
+- zwrócić z metody listę zserializowanych obiektów
+"""

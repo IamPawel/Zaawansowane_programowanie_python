19f9b1d4cda9960e9d28dc71e5d22c2d0e38920d
IamPawel <150140921+IamPawel@users.noreply.github.com>
2024-05-07 16:38:01 +0200

    dodanie progress bara

--- consumer.py
+++ consumer.py
@@ -1,6 +1,37 @@
 import time
 
 
+def printProgressBar(
+    iteration,
+    total,
+    prefix="",
+    suffix="",
+    decimals=1,
+    length=100,
+    fill="█",
+    printEnd="\r",
+):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + "-" * (length - filledLength)
+    print(f"\r{prefix} |{bar}| {percent}% {suffix}", end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
 def process_tasks(filename):
     with open(filename, "r") as f:
         for line in f:
@@ -11,11 +42,18 @@ def process_tasks(filename):
                 print(f"Wykonywanie zadania o id: {id}")
                 timestamp = taskinfo[1]
                 change_status(filename, id, "in_progress")
-                for remaining in range(30, 0, -1):
+                items = list(range(30, 0, -1))
+                l = len(items)
+                # Initial call to print 0% progress
+                printProgressBar(0, 1, prefix="Progress:", suffix="Complete", length=50)
+                for i, item in enumerate(items):
+                    # Do stuff...
                     time.sleep(1)
-                    print(
-                        f"Time remaining for task {id}: {remaining} seconds", end="\r"
+                    # Update Progress Bar
+                    printProgressBar(
+                        i + 0.1, l, prefix="Progress:", suffix="Complete", length=50
                     )
+
                 change_status(filename, id, "done")
                 break
             else:
